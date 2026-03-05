eb5612822ee3a8dcbb9c9df34423b2393cd7a3fe
Davi <davivinici71@gmail.com>
2026-03-05 16:55:01 -0300

    feat: exemplo.java

---
+++ exemplo.java
@@ -0,0 +1,10 @@
+public class exemplo {
+    public static void main(String[] args){
+        int x;
+        double y;
+        x = 5;
+        y = 2 * x;
+
+        System.out.println(y);
+    }
+}
