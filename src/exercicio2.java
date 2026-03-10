ac3fb465a5d202b7f105266258f7291699ab26b6
Davi <davivinici71@gmail.com>
2026-03-09 22:20:48 -0300

    Update: estrutura de pastas mudadas

---
+++ src/exercicio2.java
@@ -0,0 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class exercicio2{
+    public static void main(String[] args){
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+    double largura, altura, valorMetroQuadrado, area, preco;
+    largura = sc.nextDouble();
+    altura = sc.nextDouble();
+    valorMetroQuadrado = sc.nextDouble();
+    area = largura * altura;
+    preco = area * valorMetroQuadrado;
+        
+    System.out.printf("AREA = %.2f\n", area);
+    System.out.printf("PRECO = %.2f\n", preco);
+    sc.close();
+    }
+}
