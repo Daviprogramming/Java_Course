eb6cab2b71a3d0e46636a0809c156fdcee5c564a
Davi <davivinici71@gmail.com>
2026-03-10 23:11:26 -0300

    exercicio 2 realizado de uma forma igual ao do professor :D

--- src/Repetitivas/ex2.java
+++ src/Repetitivas/ex2.java
@@ -1,3 +1,31 @@
-void main() {
+package Repetitivas;
+// Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema
+//cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
+//menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).
+import java.util.Scanner;
 
+public class ex2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+        while(x != 0 && y!= 0){
+             if(x > 0 && y > 0){
+                System.out.println("Q1");
+             }
+             else if(x > 0 && y < 0){
+                 System.out.println("Q4");
+             }
+             else if(x < 0 && y > 0){
+                 System.out.println("Q2");
+             }
+             else{
+                 System.out.println("Q3");
+             }
+             x = sc.nextDouble();
+             y = sc.nextDouble();
+        }
+
+        sc.close();
+    }
 }
