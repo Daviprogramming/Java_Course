7f23e2dae09d77e4852127430e62f391e2b35d1a
Davi <davivinici71@gmail.com>
2026-03-15 02:20:20 -0300

    add: entities Triangle, update: aula2

--- src/Poo/application/aula2.java
+++ src/Poo/application/aula2.java
@@ -1,27 +1,29 @@
-package Poo;
+package Poo.application;
 
 import java.util.Scanner;
-
+import Poo.entities.Triangle;
 import static java.lang.Math.sqrt;
 
 public class aula2 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        double aX, bX, cX;
-        double aY, bY, cY;
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
+
         double p,p2, area, area2;
         System.out.println("Enter the measures of triangle X:");
-        aX = sc.nextDouble();
-        bX = sc.nextDouble();
-        cX = sc.nextDouble();
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
         System.out.println("Enter the measures of triangle Y:");
-        aY = sc.nextDouble();
-        bY = sc.nextDouble();
-        cY = sc.nextDouble();
-        p = (aX + bX + cX) / 2;
-        area = sqrt(p * (p-aX)* (p-bX)*(p-cX));
-        p2 = (aY + bY + cY) / 2;
-        area2 = sqrt(p2 * (p2-aY)* (p2-bY)*(p2-cY));
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+        p = (x.a + x.b + x.c) / 2;
+        area = sqrt(p * (p-x.a)* (p-x.b)*(p-x.c));
+        p2 = (y.a + y.b + y.c) / 2;
+        area2 = sqrt(p2 * (p2-y.a)* (p2-y.b)*(p2-y.c));
 
         System.out.printf("Triangle X area: %.4f\n", area);
         System.out.printf("Triangle Y area: %.4f\n", area2);
